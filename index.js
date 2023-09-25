22dd934548d0423c72c60aed5c4bffc79b85173a
Abhishek Vyas <abhishekvyas30008@gmail.com>
2023-09-25 16:44:22 +0530

    change port and make clickable redirect link

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const path = require("path");
 const  connect = require("./configs/db");
 const urlController = require("./controllers/url-controller");
 const staticController = require("./controllers/static-controller");
-const PORT = 8000;
+const PORT = 8008;
 const app = express();
 
 app.use(express.json());
@@ -18,7 +18,7 @@ app.use("/", staticController);
 app.listen(PORT, async () => {
     try {
       await connect();
-      console.log("listening to port 8000...");
+      console.log("listening to port 8008...");
     } catch (error) {
       console.log("error:", error);
     }
